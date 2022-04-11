462c98e05670ffcecdabe696867473284aecc8d0
nc <nelson.ncdesigns@gmail.com>
2022-04-10 23:10:38 -0400

    Init stylus file w/Boostrap & Normalize CSS. Arrays for random message components.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import './css/styles.styl'
 import $ from "jquery"
 
-var jq = $.noConflict();
+const jq = $.noConflict(),
+quotes = [
+    `I will build a ${adj}, ${adj} ${noun} on our southern border, and I will have Mexico ${verb} for that ${noun}. Mark my words.`,
+    `I have a ${adj} relationship with ${noun}s, as you possibly have heard. I just have ${adj} respect for them. And they ${verb} me. I ${verb} them.`,
+    `To be ${adj}, ${noun}s would ${verb} for me. They just would. Why? Maybe because I'm so ${adj} looking.`
+],
+nouns=['wall','burder','covfefe','MAGA','loser','African American'],
+verbs=['shutdown','pay','covfefe'],
+adjs=['huge','fake','disgusting','biggest'];
+let verb='',
+noun='',
+adj='';
+
 (function($){
 
-    console.log('test');
+    
     
 })(jq);
+
+//I will build a great, great [noun] on our southern border, and I will have Mexico [verb] for that [noun]. Mark my words.
+//I have a great relationship with [noun]s, as you possibly have heard. I just have great respect for them. And they [verb] me. I [verb] them.
+//To be blunt, [noun]s would [verb] for me. They just would. Why? Maybe because I'm so good looking.
